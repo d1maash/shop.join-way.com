60b1cb791823ab26817c3d63fb108ac1e2504675
Dimash <dimashaijarikov@gmail.com>
2024-05-13 10:34:57 +0500

    add nav and form

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,16 @@
-$(function() {
+const sidenav = document.getElementById("sidenav-1");
+const sidenavInstance = mdb.Sidenav.getInstance(sidenav);
 
-  var siteSticky = function() {
-		$(".js-sticky-header").sticky({topSpacing:0});
-	};
-	siteSticky();
+let innerWidth = null;
 
-	var siteDropdown = function() {
+const setMode = (e) => {
+// Check necessary for Android devices
+if (window.innerWidth === innerWidth) {
+return;
+}
 
-		$('nav .dropdown').hover(function(){
-			var $this = $(this);
-			$this.addClass('show');
-			$this.find('> a').attr('aria-expanded', true);
-			$this.find('.dropdown-menu').addClass('show');
-		}, function(){
-			var $this = $(this);
-				$this.removeClass('show');
-				$this.find('> a').attr('aria-expanded', false);
-				$this.find('.dropdown-menu').removeClass('show');
-		});
+innerWidth = window.innerWidth;
 
-
-		$('#dropdown04').on('show.bs.dropdown', function () {
-		  console.log('show');
-		});
-
-	  $('.navbar .dropdown > a').click(function(){
-	    location.href = this.href;
-	  });
-	}; 
-	siteDropdown();
-
-});
+if (window.innerWidth < 1400) { sidenavInstance.changeMode("over"); sidenavInstance.hide();
+  } else { sidenavInstance.changeMode("side"); sidenavInstance.show(); } }; setMode(); //
+window.addEventListener("resize", setMode);
